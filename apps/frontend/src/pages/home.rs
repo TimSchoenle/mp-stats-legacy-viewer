98e3d613f0c161ae0d99a96c80923413830d6dc0
Tim Schönle <13711365+timschoenle@users.noreply.github.com>
2026-02-18 19:04:10 +0100

    feat: add a prototype warning on the home page

--- apps/frontend/src/pages/home.rs
+++ apps/frontend/src/pages/home.rs
@@ -6,7 +6,8 @@ use yew_router::prelude::*;
 pub fn home() -> Html {
     html! {
         <div class="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-            <h1 class="text-5xl font-bold mb-8">{ "Welcome to Stats Viewer" }</h1>
+            <h1 class="text-5xl font-bold mb-8">{ "MP Stats Legacy Viewer" }</h1>
+            <p>{"This is an early prototype of the legacy data collected by the StatsBot. This page will crash and other stuff."}</p>
             <div class="flex space-x-8">
                 <Link<Route> to={Route::JavaLanding} classes="p-10 bg-green-600 rounded-lg shadow-xl hover:bg-green-500 transition transform hover:scale-105">
                     <h2 class="text-3xl font-bold">{ "Java Edition" }</h2>
